8630a72297c516818c7e9d553fe4bb6fbec6dc39
Garo1995 <garikarshakyan3@gmail.com>
2026-07-04 01:25:07 +0400

    aasdwe

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,33 @@ $(document).ready(function() {
 });
 
 
+$(document).ready(function () {
+    setTimeout(function () {
+        $(".stack").addClass("stack-anime");
+    }, 800);
+
+});
+
+const revealOnScroll = () => {
+    const elementsToReveal = document.querySelectorAll('.animate-on-scroll');
+    const triggerPoint = window.innerHeight * 0.85;
+
+    elementsToReveal.forEach((element) => {
+        const elementTop = element.getBoundingClientRect().top;
+
+        if (elementTop < triggerPoint) {
+            element.classList.add('visible');
+        }
+    });
+};
+
+// 🔄 Attach listeners
+window.addEventListener('scroll', revealOnScroll);
+window.addEventListener('load', revealOnScroll);
+
+
+
+
 gsap.registerPlugin(ScrollTrigger);
 
 ScrollTrigger.config({
